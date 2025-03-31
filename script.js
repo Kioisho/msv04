f3e3e6c84d4b1aa24bdbdc6cb232b9564ee848e3
Kioisho <obedismaelh@gmail.com>
2025-03-30 23:28:22 -0600

    v2.0

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggleMoveBtn = document.getElementById("toggleMove");
 const toggleFlipBtn = document.getElementById("toggleFlip");
 const brownBox = document.querySelector(".brown-box");
+const whiteBox = document.querySelector(".white-box");
 const hiddenText = document.querySelector(".hidden-text");
 const hiddenImage = document.querySelector(".hidden-image");
 
@@ -11,13 +12,13 @@ let flipped = false;
 toggleMoveBtn.addEventListener("click", () => {
     if (!moved) {
         brownBox.style.bottom = "0px";
-        brownBox.style.bottom = "-650px"; // Se mueve hacia abajo
+        whiteBox.style.bottom = "650px"; // Se mueve hacia arriba
         hiddenText.style.opacity = "1"; // Muestra el texto
         toggleMoveBtn.textContent = "Ocultar";
     } else {
-        brownBox.style.bottom = "-10px"; // Regresa a su posición original
+        whiteBox.style.bottom = "10px"; // Regresa a su posición original
         hiddenText.style.opacity = "0"; // Oculta el texto
-        toggleMoveBtn.textContent = "Mostrar más";
+        toggleMoveBtn.textContent = "Abrir";
     }
     moved = !moved;
 });
